c7857021914c440e95a8bb00535e459e2cd36fca
Ugur Erdemir <ugur.erdemir@mercedes-benz.com>
2022-10-29 11:22:11 +0200

    test

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -1,31 +1,31 @@
-import * as dotenv from "dotenv";
-import cors from "cors";
-import express from "express";
-import { sequelize } from "./sequelize";
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import express from 'express';
+import { sequelize } from './sequelize';
 
-import { IndexRouter } from "./controllers/v0/index.router";
+import { IndexRouter } from './controllers/v0/index.router';
 
-import bodyParser from "body-parser";
-import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
+import bodyParser from 'body-parser';
+import { V0_FEED_MODELS, V0_USER_MODELS } from './controllers/v0/model.index';
 
 (async () => {
   dotenv.config();
 
   try {
     await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
+    console.log('Connection has been established successfully.');
   } catch (error) {
-    console.error("Unable to connect to the database:", error);
+    console.error('Unable to connect to the database:', error);
   }
 
   await sequelize.addModels(V0_FEED_MODELS);
   await sequelize.addModels(V0_USER_MODELS);
   await sequelize.sync();
 
-  console.log("Database Connected");
+  console.log('Database Connected');
 
   const app = express();
-  const port = process.env.PORT || 8080;
+  const port = 8080;
 
   app.use(bodyParser.json());
 
@@ -45,11 +45,11 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
   //   "origin": '*',
   // }));
 
-  app.use("/api/v0/", IndexRouter);
+  app.use('/api/v0/', IndexRouter);
 
   // Root URI call
-  app.get("/", async (req, res) => {
-    res.send("/api/v0/");
+  app.get('/', async (req, res) => {
+    res.send('/api/v0/');
   });
 
   // Start the Server
